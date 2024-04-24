4fb69bc27c628ed509d1ef48a0ee62cf4ea704a3
Fabrice Mpozenzi <fxm20c@acu.edu>
2024-04-24 12:41:22 -0500

    Hi this is my first commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,11 +1,7 @@
 //System.out.println("Hi");
 public class Hello {
     public static void main(String[] args){
-        int count = 0;
-        while(args.length!=0){
-            System.out.println(args[count]);
-            count++;
-        }
+        System.out.println("Hello World");
     }
 
     
